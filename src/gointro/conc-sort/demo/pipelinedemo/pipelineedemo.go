952f55fbf6a654e347e96905e3e8acf1d0e359f8
YourtionGuo <yourtion@gmail.com>
2018-07-14 17:04:21 +0800

    add Merge

--- src/gointro/conc-sort/demo/pipelinedemo/pipelineedemo.go
+++ src/gointro/conc-sort/demo/pipelinedemo/pipelineedemo.go
@@ -6,8 +6,15 @@ import (
 )
 
 func main() {
-	p := pipeline.InMemSort(
-		pipeline.ArraySource(3, 2, 6, 7, 4))
+
+	p := pipeline.Merge(
+		pipeline.InMemSort(
+			pipeline.ArraySource(3, 2, 6, 7, 4),
+		),
+		pipeline.InMemSort(
+			pipeline.ArraySource(7, 4, 0, 3, 2, 13, 8),
+		),
+	)
 	for v := range p {
 		fmt.Println(v)
 	}
