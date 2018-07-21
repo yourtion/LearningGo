4876f66a82118fc2905c47a1612377d44199ab67
YourtionGuo <yourtion@gmail.com>
2018-07-21 11:21:04 +0800

    3-5 Map例题

---
+++ src/learngo/basic/nonrepeating.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func lengthOfNonRepeatingSubStr(s string) int {
+	lastOccurred := make(map[byte]int)
+	start := 0
+	maxLength := 0
+
+	for i, ch := range []byte(s) {
+		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+			start = lastOccurred[ch] + 1
+		}
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
+	}
+
+	return maxLength
+}
+
+func main() {
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("abaabcbb"))
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("bbbbb"))
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("pwwkwe"))
+	fmt.Println(
+		lengthOfNonRepeatingSubStr(""))
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("b"))
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("abcdef"))
+
+	// TODO fix non-ascii
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("测试测试"))
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("一二三一二"))
+}
