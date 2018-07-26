04f80c6560022be63db010d28741ada7373a4a79
YourtionGuo <yourtion@gmail.com>
2018-07-26 23:48:24 +0800

    6-2 函数式编程例一

---
+++ src/learngo/functional/fib/fib.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+// 1, 1, 2, 3, 5, 8, 13, 21
+//    a, b
+//       a, b
+func fibonacci() intGen {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+type intGen func() int
+
+func (g intGen) Read(p []byte) (n int, err error) {
+	next := g()
+	if next > 10000 {
+		return 0, io.EOF
+	}
+	s := fmt.Sprintf("%d\n", next)
+
+	// TODO: incorrect if p is oo small!
+	return strings.NewReader(s).Read(p)
+}
+
+func printFileContents(reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func main() {
+	f := fibonacci()
+
+	//fmt.Println(f()) // 1
+	//fmt.Println(f()) // 1
+	//fmt.Println(f()) // 2
+	//fmt.Println(f()) // 3
+	//fmt.Println(f()) // 5
+	//fmt.Println(f()) // 8
+	//fmt.Println(f()) // 13
+	//fmt.Println(f()) // 21
+
+	printFileContents(f)
+}
