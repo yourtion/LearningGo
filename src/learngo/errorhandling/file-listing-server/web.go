5e11c05bfb3dee499f352631356b657a206df9b2
YourtionGuo <yourtion@gmail.com>
2018-07-29 16:13:27 +0800

    7-3 服务器统一错误处理

---
+++ src/learngo/errorhandling/file-listing-server/web.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"learngo/errorhandling/file-listing-server/filelisting"
+	"log"
+	"net/http"
+	"os"
+)
+
+type appHandler func(writer http.ResponseWriter, request *http.Request) error
+
+func errWrapper(
+	handler appHandler) func(
+	http.ResponseWriter, *http.Request) {
+	return func(writer http.ResponseWriter,
+		request *http.Request) {
+		err := handler(writer, request)
+		if err != nil {
+			log.Printf("Error handing request: %s\n", err.Error())
+			code := http.StatusOK
+			switch {
+			case os.IsNotExist(err):
+				code = http.StatusNotFound
+			case os.IsPermission(err):
+				code = http.StatusForbidden
+			default:
+				code = http.StatusInternalServerError
+			}
+			http.Error(writer, http.StatusText(code), code)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/list/",
+		errWrapper(
+			filelisting.HandlerFileLiet))
+
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		panic(err)
+	}
+}
