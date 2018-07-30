d3ccab91e736270289a63450b4325d1a30cd4dc1
YourtionGuo <yourtion@gmail.com>
2018-07-30 22:43:57 +0800

    8-2 代码覆盖率和性能测试

--- src/learngo/container/nonrepeating/nonrepeating_test.go
+++ src/learngo/container/nonrepeating/nonrepeating_test.go
@@ -2,6 +2,9 @@ package main
 
 import "testing"
 
+// go test -coverprofile=c.out
+// go tool cover -html=c.out
+
 func TestSubstr(t *testing.T) {
 
 	tests := []struct {
@@ -21,12 +24,28 @@ func TestSubstr(t *testing.T) {
 		// Chinese support
 		{"测试测试", 2},
 		{"一二三一二", 3},
+		{"黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花", 8},
 	}
 
 	for _, tt := range tests {
 		actual := lengthOfNonRepeatingSubStr(tt.s)
 		if actual != tt.ans {
 			t.Errorf("lengthOfNonRepeatingSubStr(%s); got %d; expected %d", tt.s, actual, tt.ans)
+		}
+	}
+}
+
+//  go test -bench .
+
+func BenchmarkSubstr(b *testing.B) {
+
+	s := "黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"
+	ans := 8
+
+	for i := 0; i < b.N; i++ {
+		actual := lengthOfNonRepeatingSubStr(s)
+		if actual != ans {
+			b.Errorf("lengthOfNonRepeatingSubStr(%s); got %d; expected %d", s, actual, ans)
 		}
 	}
 }
