88bdb37f767c2ce35ca2c7b9b7dedd1e3046309b
YourtionGuo <yourtion@gmail.com>
2018-08-08 23:28:13 +0800

    10-4 使用select进行调度

--- src/learngo/channel/select/select.go
+++ src/learngo/channel/select/select.go
@@ -22,16 +22,56 @@ func generator() chan int {
 	return out
 }
 
+func worker(id int, c <-chan int) {
+	for n := range c {
+		time.Sleep(time.Second)
+		fmt.Printf("Worker %d recevved %d \n", id, n)
+	}
+}
+
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+	return c
+}
+
 func main() {
 	var c1, c2 = generator(), generator()
+	worker := createWorker(0)
+
+	// 缓冲 value
+	var values []int
+
+	tm := time.After(10 * time.Second)
+	tick := time.Tick(time.Second)
+
 	for {
+		var activeWorker chan<- int
+		var activeValues int
+		if len(values) > 0 {
+			activeWorker = worker
+			activeValues = values[0]
+		}
+
 		select {
+
 		case n := <-c1:
-			fmt.Println("Received from c1:", n)
+			values = append(values, n)
 		case n := <-c2:
-			fmt.Println("Received from c2:", n)
-			//default:
-			//	fmt.Println("No values received")
+			values = append(values, n)
+		case activeWorker <- activeValues:
+			values = values[1:]
+
+		case <-time.After(800 * time.Millisecond):
+			// no data in 800ms
+			fmt.Println("timeout")
+		case <-tick:
+			// report queue length every 1s
+			fmt.Println("queue len = ", len(values))
+		case <-tm:
+			// run 10s and exit
+			fmt.Println("Bye.")
+			return
 		}
 	}
 }
