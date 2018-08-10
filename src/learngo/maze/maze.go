cbb713b36ee83b02067d90effc6b829c257f2512
YourtionGuo <yourtion@gmail.com>
2018-08-10 23:37:12 +0800

    12-2 迷宫代码实现（2）

--- src/learngo/maze/maze.go
+++ src/learngo/maze/maze.go
@@ -25,12 +25,79 @@ func readMaze(filename string) [][]int {
 	return maze
 }
 
+type point struct {
+	i, j int
+}
+
+var dirs = [4]point{
+	{-1, 0}, {0, -1}, {1, 0}, {0, 1}}
+
+func (p point) add(r point) point {
+	return point{p.i + r.i, p.j + r.j}
+}
+
+func (p point) at(grid [][]int) (int, bool) {
+	if p.i < 0 || p.i >= len(grid) {
+		return 0, false
+	}
+
+	if p.j < 0 || p.j >= len(grid[p.i]) {
+		return 0, false
+	}
+	return grid[p.i][p.j], true
+}
+
+func walk(maze [][]int, start point, end point) [][]int {
+	steps := make([][]int, len(maze))
+	for i := range steps {
+		steps[i] = make([]int, len(maze[i]))
+	}
+	Q := []point{start}
+
+	for len(Q) > 0 {
+		cur := Q[0]
+		Q = Q[1:]
+
+		if cur == end {
+			break
+		}
+
+		for _, dir := range dirs {
+			next := cur.add(dir)
+
+			// maze at next is 0
+			val, ok := next.at(maze)
+			if !ok || val == 1 {
+				continue
+			}
+
+			// and step at next is 0
+			val, ok = next.at(steps)
+			if !ok || val != 0 {
+				continue
+			}
+
+			// and next != start
+			if next == start {
+				continue
+			}
+
+			curSteps, _ := cur.at(steps)
+			steps[next.i][next.j] = curSteps + 1
+			Q = append(Q, next)
+		}
+	}
+	return steps
+}
+
 func main() {
 	maze := readMaze("src/learngo/maze/maze.txt")
 
-	for _, row := range maze {
+	res := walk(maze, point{0, 0}, point{len(maze) - 1, len(maze[0]) - 1})
+
+	for _, row := range res {
 		for _, val := range row {
-			fmt.Printf("%d ", val)
+			fmt.Printf("%3d ", val)
 		}
 		fmt.Println()
 	}
