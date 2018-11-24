688aa75cf84eb02b6e68ddd5fcebdcec9e715c82
YourtionGuo <yourtion@gmail.com>
2018-11-24 16:16:52 +0800

    14-5 Engine与Parser

---
+++ src/learngo/crawler/engine/engine.go
@@ -0,0 +1,36 @@
+package engine
+
+import (
+	"learngo/crawler/fetcher"
+	"log"
+)
+
+func Run(seeds ...Request) {
+
+	var requests []Request
+	for _, r := range seeds {
+		requests = append(requests, r)
+	}
+
+	for len(requests) > 0 {
+		r := requests[0]
+		requests = requests[1:]
+
+		log.Printf("Fetching %s", r.Url)
+		body, err := fetcher.Fetch(r.Url)
+		if err != nil {
+			log.Printf("Fetcher: error "+
+				"fetching url %s: %v", r.Url, err)
+			continue
+		}
+
+		parserResult := r.ParserFunc(body)
+
+		requests = append(requests, parserResult.Requests...)
+
+		for _, item := range parserResult.Items {
+			log.Printf("Got item %v", item)
+
+		}
+	}
+}
--- src/learngo/crawler/main.go
+++ src/learngo/crawler/main.go
@@ -1,56 +1,14 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/transform"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"regexp"
+	"learngo/crawler/engine"
+	"learngo/crawler/zhenai/parser"
 )
 
 func main() {
-	resp, err := http.Get(
-		"http://www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error: status code",
-			resp.StatusCode)
-		return
-	}
-
-	e := determineEncoding(resp.Body)
-	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
-	all, err := ioutil.ReadAll(utf8Reader)
-	if err != nil {
-		panic(err)
-	}
-
-	printCityList(all)
-}
-
-func printCityList(contents []byte) {
-	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
-	matches := re.FindAllSubmatch(contents, -1)
-	for _, m := range matches {
-		fmt.Printf("City: %s, URL: %s \n", m[2], m[1])
-	}
-	fmt.Printf("Matches fount: %d\n", len(matches))
-}
-
-func determineEncoding(r io.Reader) encoding.Encoding {
-	bytes, err := bufio.NewReader(r).Peek(1024)
-	if err != nil {
-		panic(err)
-	}
-
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
+	engine.Run(engine.Request{
+		Url:        "http://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
+	})
 }
