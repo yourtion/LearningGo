8a00fca573c5bce29cce649320bebac9a23561e7
YourtionGuo <yourtion@gmail.com>
2019-02-24 11:53:05 +0800

    lottery(大转盘): 使用 atomic.AddInt32 替换互斥锁提升性能

--- src/lottery/_demo/6-wheel/main.go
+++ src/lottery/_demo/6-wheel/main.go
@@ -6,17 +6,17 @@ import (
 	"github.com/kataras/iris/mvc"
 	"math/rand"
 	"strings"
-	"sync"
+	"sync/atomic"
 	"time"
 )
 
 // 奖品中奖概率
 type prizeRate struct {
-	Rate  int // 万分之N的中奖概率
-	Total int // 总数量限制，0 表示无限数量
-	CodeA int // 中奖概率起始编码（包含）
-	CodeB int // 中奖概率终止编码（包含）
-	Left  int // 剩余数
+	Rate  int    // 万分之N的中奖概率
+	Total int    // 总数量限制，0 表示无限数量
+	CodeA int    // 中奖概率起始编码（包含）
+	CodeB int    // 中奖概率终止编码（包含）
+	Left  *int32 // 剩余数
 }
 
 // 奖品列表
@@ -28,14 +28,13 @@ var prizeList = []string{
 }
 
 // 奖品的中奖概率设置，与上面的 prizeList 对应的设置
+var left = int32(1000)
 var rateList = []prizeRate{
-	{100, 1000, 0, 9999, 1000},
+	{100, 1000, 0, 9999, &left},
 	//{20, 2, 1, 2, 2},
 	//{50, 10, 3, 5, 10},
 	//{1000, 0, 100, 9999, 1000},
 }
-
-var mu = sync.Mutex{}
 
 type lotteryController struct {
 	Ctx iris.Context
@@ -71,39 +70,37 @@ func (c *lotteryController) GetPrize() string {
 	code := r.Intn(10000)
 
 	//fmt.Println("GetPrize code=", code)
-	var myPrize string
+	var myprize string
 	var prizeRate *prizeRate
 	// 从奖品列表中匹配，是否中奖
 	for i, prize := range prizeList {
 		rate := &rateList[i]
 		if code >= rate.CodeA && code <= rate.CodeB {
 			// 满足中奖条件
-			myPrize = prize
+			myprize = prize
 			prizeRate = rate
 			break
 		}
 	}
-
-	if myPrize == "" {
+	if myprize == "" {
 		// 没有中奖
-		myPrize = "很遗憾，再来一次"
-		return myPrize
+		myprize = "很遗憾，再来一次"
+		return myprize
 	}
 	// 第二步，发奖，是否可以发奖
 	if prizeRate.Total == 0 {
 		// 无限奖品
-		fmt.Println("中奖： ", myPrize)
-		return myPrize
-	} else if prizeRate.Left > 0 {
-		// 还有剩余奖品
-		mu.Lock()
-		prizeRate.Left -= 1
-		mu.Unlock()
-		fmt.Println("中奖： ", myPrize)
-		return myPrize
-	} else {
-		// 有限且没有剩余奖品，无法发奖
-		myPrize = "很遗憾，再来一次"
-		return myPrize
+		fmt.Println("中奖： ", myprize)
+		return myprize
+	} else if *prizeRate.Left > 0 {
+		// 还有剩余奖品，使用 CAS 操作来做安全更新
+		left := atomic.AddInt32(prizeRate.Left, -1)
+		if left >= 0 {
+			fmt.Println("中奖： ", myprize)
+			return myprize
+		}
 	}
+	// 有限且没有剩余奖品，无法发奖
+	myprize = "很遗憾，再来一次"
+	return myprize
 }
